b4674aa4536f63320dbe3c51d7f3c4ca5a3affb4
karthikprabhu04 <kpp32@cam.ac.uk>
2025-07-30 14:43:38 +0100

    Change functions to classes

--- books.js
+++ books.js
@@ -1,100 +1,138 @@
-const myLibrary = [];
+// Create Library class
+class Library {
+  constructor() {
+    this.books = [];
+  }
 
-function Book(title, author, pages, read) {
-    this.id = crypto.randomUUID()
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    addBookToLibrary(this);
+  addBook(book) {
+    this.books.push(book);
+  }
+
+  removeBookById(id) {
+    this.books = this.books.filter(book => book.id !== id)
+  }
+
 }
 
+// Create Book class
+class Book {
+  constructor(title, author, pages, read) {
+  this.id = crypto.randomUUID();
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+  }
 
-function addBookToLibrary(book) {
-  myLibrary.push(book)
+  toggleRead() {
+    this.read = this.read === "READ" ? "NOT READ" : "READ";
+  }
 }
 
-// Display items on html page
-new Book("The Alchemist", "Paulo Coelho", "208", "READ");
-new Book("The Lean Startup", "Eric Ries", "336", "READ");
-new Book("Zero to One", "Peter Thiel", "224", "NOT READ");
+// Create library interface
+class LibraryUI {
+  constructor(library) {
+    this.library = library;
+  }
 
-const list = document.querySelector(".list");
+  render() {
+    const list = document.querySelector(".list");
+    list.innerHTML="";
 
-function renderLibrary() {
-  list.innerHTML = "";
-  myLibrary.forEach(book => {
-    const item = document.createElement("tr");
-    // item.textContent = `${book.title} ${book.author} ${book.pages} ${book.read} (${book.id})`;
-     item.innerHTML = `
-      <td>${book.title}</td>
-      <td>${book.author}</td>
-      <td>${book.pages}</td>
-      <td>${book.read}</td>
-      <td>${book.id}</td>
-      <td class="actions"></td>
-    `;
+    this.library.books.forEach(book => {
+      const item = document.createElement("tr");
+      item.dataset.id = book.id;
 
-    item.dataset.id = book.id;
-    list.appendChild(item);
+      item.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.pages}</td>
+        <td>${book.read}</td>
+        <td>${book.id}</td>
+        <td class="actions"></td>
+      `;
 
-    const actionsCell = item.querySelector("td.actions")
+      const actionsCell = item.querySelector("td.actions");
 
-    // Remove button
-    const remove = document.createElement("button");
-    remove.textContent = "Remove";
-    remove.addEventListener("click", (e) => {
-      item.remove();
-      e.target.remove();
-      const index = myLibrary.findIndex(book => item.dataset.id === book.id);
-      if (index !== -1) {
-        myLibrary.splice(index, 1);
-      }
+      // Remove button
+      const removeBtn = document.createElement("button");
+      removeBtn.textContent = "Remove";
+      removeBtn.addEventListener("click", () => {
+        this.library.removeBookById(book.id);
+        this.render();
+      })
+      actionsCell.appendChild(removeBtn);
+
+      // Toggle button
+      const toggleBtn = document.createElement("button");
+      toggleBtn.textContent = "Toggle Read Status";
+      toggleBtn.addEventListener("click", () => {
+        book.toggleRead();
+        this.render();
+      })
+      actionsCell.appendChild(toggleBtn)
+
+      list.appendChild(item);
     })
-    actionsCell.appendChild(remove);
-
-    // Toggle read button
-    const read = document.createElement("button");
-    read.textContent = "Toggle Read Status";
-    read.addEventListener("click", () => {
-      const index = myLibrary.findIndex(book => item.dataset.id === book.id);
-      if (index !== -1) {
-        myLibrary[index].read = myLibrary[index].read === "READ" ? "NOT READ" : "READ";
-        renderLibrary();
-      }
-    })
-    actionsCell.appendChild(read)
-  });
+  }
 }
-
-renderLibrary();
 
 // Add New Book button
-const addBook = document.querySelector("#add-book");
-const dialog = document.querySelector("#dialog");
-const bookForm = document.querySelector("#book-form");
-const cancel = document.querySelector("#cancel");
+class BookFormUI {
+  constructor(library, ui) {
+    this.library = library;
+    this.ui = ui;
 
-addBook.addEventListener("click", () => {
-  dialog.showModal();
-});
+    this.initListeners();
+  }
 
-cancel.addEventListener("click", () => {
-  dialog.close()
-});
+  initListeners() {
+    const addBook = document.querySelector("#add-book");
+    const dialog = document.querySelector("#dialog");
+    const bookForm = document.querySelector("#book-form");
+    const cancel = document.querySelector("#cancel");
+    
+    addBook.addEventListener("click", () => {
+      dialog.showModal();
+    });
+    
+    cancel.addEventListener("click", () => {
+      dialog.close()
+    });
+    
+    bookForm.addEventListener("submit", (e) => {
+      e.preventDefault();
+      this.handleSubmit();
+    })
+  }
+  
+  handleSubmit() {
+    const dialog = document.querySelector("#dialog");
+    const bookForm = document.querySelector("#book-form");
 
-bookForm.addEventListener("submit", (e) => {
-  e.preventDefault();
+    const formdata = new FormData(bookForm);
+    const title = formdata.get("title");
+    const author = formdata.get("author");
+    const pages = formdata.get("pages");
+    const read = formdata.get("read") ? "READ" : "NOT READ";
 
-  const formdata = new FormData(bookForm);
-  const title = formdata.get("title");
-  const author = formdata.get("author");
-  const pages = formdata.get("pages");
-  const read = formdata.get("read") ? "READ" : "NOT READ";
+    const newBook = new Book(title, author, pages, read);
+    this.library.addBook(newBook);
+    this.ui.render();
 
-  new Book(title, author, pages, read)
-  renderLibrary();
+    bookForm.reset();
+    dialog.close();
+  }
+}
 
-  bookForm.reset();
-  dialog.close()
-});
+
+// Add books to library
+const myLibrary = new Library();
+const ui = new LibraryUI(myLibrary);
+const formUI = new BookFormUI(myLibrary, ui);
+
+myLibrary.addBook(new Book("The Alchemist", "Paulo Coelho", "208", "READ"));
+myLibrary.addBook(new Book("The Lean Startup", "Eric Ries", "336", "READ"));
+myLibrary.addBook(new Book("Zero to One", "Peter Thiel", "224", "NOT READ"));
+
+ui.render();
